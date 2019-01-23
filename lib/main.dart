ad6f5b85eae80217ff6b6fcaca9806ac57780057
suinua <maatinosippo@gmail.com>
2019-01-23 17:56:09 +0900

    タイトルの修正

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Dartask',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
